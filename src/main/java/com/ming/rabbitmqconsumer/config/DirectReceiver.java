83fc22446424c9df1ae6b6bfca0390a01f017598
liming <522724933@qq.com>
2022-02-15 11:04:54 +0800

    first commit

---
+++ src/main/java/com/ming/rabbitmqconsumer/config/DirectReceiver.java
@@ -0,0 +1,20 @@
+package com.ming.rabbitmqconsumer.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 监听类
+ */
+@Component
+@RabbitListener(queues = "directQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
